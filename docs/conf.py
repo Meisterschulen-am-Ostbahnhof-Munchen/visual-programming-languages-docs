0f898cc77a17d013106795c59c650ee32aef0440
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-01-13 18:23:45 +0100

    tst

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -13,39 +14,62 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+SOURCE_DIR = os.path.abspath(os.path.join('.', '..', 'src'))
+sys.path.insert(0, SOURCE_DIR)
 
 
 # -- Project information -----------------------------------------------------
 
-project = "visual programming languages Docs"
-copyright = "2022, Meisterschulen am Ostbahnhof, München"
-author = "Franz Höpfinger"
+project = 'visual programming languages Docs'
+copyright = '2022, Meisterschulen am Ostbahnhof, München'
+author = 'Franz Höpfinger'
 
 
 # -- General configuration ---------------------------------------------------
-# -- General configuration
 
+# If your documentation needs a minimal Sphinx version, state it here.
+#
+# needs_sphinx = '1.0'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.imgconverter",
-    "recommonmark",
-    "sphinxcontrib.mermaid",
-    "sphinx_markdown_tables"
+    'sphinx.ext.autodoc',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'sphinxcontrib.autoprogram',
+    'sphinx_c_autodoc',
+    'sphinx_c_autodoc.napoleon',
+    'sphinx_c_autodoc.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.imgconverter',
+    'recommonmark',
+    'sphinxcontrib.mermaid',
+    'sphinx_markdown_tables'
+    # Unfortunatly the typehints fail for the autodoc Documenter class, would
+    # really like to work around this, but also want to keep the warning output
+    # clean since this is a sphinx extension and would like to know when/if it
+    # is producing warnings of its own
+    # 'sphinx_autodoc_typehints',
 ]
 
-version = "0.0.1"
+version = '0.0.1'
 release = version
 intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    'rtd': ('https://docs.readthedocs.io/en/stable/', None),
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ["std"]
+intersphinx_disabled_domains = ['std']
 
 # -- General configuration ---------------------------------------------------
 
@@ -57,58 +81,41 @@ intersphinx_disabled_domains = ["std"]
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-# Handle failing hawkmoth import gracefully to be able to build the
-# documentation on e.g. https://readthedocs.org/ which would otherwise fail due
-# to missing clang.
 
-# This is not a good example to follow in regular documentation.
-try:
-    import hawkmoth
-    from hawkmoth.util import doccompat, readthedocs
-    from sphinx.ext.napoleon import docstring, Config
-
-    readthedocs.clang_setup()
-
-    extensions.append('hawkmoth')
-    tags.add('have_hawkmoth')
-
-    def napoleon_transform(comment):
-        config = Config(napoleon_use_rtype=False)
-        return str(docstring.GoogleDocstring(comment, config))
-
-    cautodoc_transformations = {
-        'napoleon': napoleon_transform,
-        'javadoc-liberal': doccompat.javadoc_liberal,
-    }
-except ImportError:
-    sys.stderr.write('Warning: Failed to import hawkmoth. Mocking results.\n')
-    sys.path.insert(0, os.path.abspath('ext'))
-    # The mock extension will include the hawkmoth test suite expected results
-    # into the documentation instead of generating.
-    extensions.append('automock')
-
-hawkmoth_root = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../test')
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # -- Options for EPUB output
-epub_show_urls = "footnote"
+epub_show_urls = 'footnote'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', 'CHANGELOG.rst']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# force libclang access for read the docs.
+# Read the docs installs doxygen which will install libclang, unfortunatly it
+# doesn't appear the so is in the path so need to grab a _backend_ version of
+# the file.
+if 'READTHEDOCS' in os.environ:
+    from clang import cindex
+    cindex.Config.set_library_file("/usr/lib/x86_64-linux-gnu/libclang-6.0.so.1")

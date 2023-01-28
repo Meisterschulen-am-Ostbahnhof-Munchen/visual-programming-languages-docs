f3f2240eaeafd0987face6b6876d21dedc1e1dad
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-01-28 13:44:25 +0100

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
-#
+# v
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -14,15 +13,16 @@
 #
 import os
 import sys
-SOURCE_DIR = os.path.abspath(os.path.join('.', '..', 'src'))
+
+SOURCE_DIR = os.path.abspath(os.path.join(".", "..", "src"))
 sys.path.insert(0, SOURCE_DIR)
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'visual programming languages Docs'
-copyright = '2022, Meisterschulen am Ostbahnhof, München'
-author = 'Franz Höpfinger'
+project = "visual programming languages Docs"
+copyright = "2022, Meisterschulen am Ostbahnhof, München"
+author = "Franz Höpfinger"
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,26 +35,26 @@ author = 'Franz Höpfinger'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
-    'sphinxcontrib.autoprogram',
-    'sphinx_c_autodoc',
-    'sphinx_c_autodoc.napoleon',
-    'sphinx_c_autodoc.viewcode',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.imgconverter',
-    'sphinxcontrib.mermaid',
-    'sphinx_markdown_tables'
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
+    "sphinxcontrib.autoprogram",
+    "sphinx_c_autodoc",
+    "sphinx_c_autodoc.napoleon",
+    "sphinx_c_autodoc.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.imgconverter",
+    "sphinxcontrib.mermaid",
+    "sphinx_markdown_tables"
     # Unfortunatly the typehints fail for the autodoc Documenter class, would
     # really like to work around this, but also want to keep the warning output
     # clean since this is a sphinx extension and would like to know when/if it
@@ -62,14 +62,14 @@ extensions = [
     # 'sphinx_autodoc_typehints',
 ]
 
-version = '0.0.1'
+version = "0.0.1"
 release = version
 intersphinx_mapping = {
-    'rtd': ('https://docs.readthedocs.io/en/stable/', None),
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
 # -- General configuration ---------------------------------------------------
 
@@ -82,17 +82,16 @@ intersphinx_disabled_domains = ['std']
 # ones.
 
 
-
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', 'CHANGELOG.rst']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "CHANGELOG.rst"]
 # -- MyST settings ---------------------------------------------------
 
 myst_enable_extensions = [
@@ -126,7 +125,7 @@ autosectionlabel_prefix_document = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -137,15 +136,16 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # -- Options for sphinx_c_autodoc, the extension being documented -----------
 
-c_autodoc_roots = ['../tests/assets/c_source']
+c_autodoc_roots = ["../tests/assets/c_source"]
 # force libclang access for read the docs.
 # Read the docs installs doxygen which will install libclang, unfortunatly it
 # doesn't appear the so is in the path so need to grab a _backend_ version of
 # the file.
-if 'READTHEDOCS' in os.environ:
+if "READTHEDOCS" in os.environ:
     from clang import cindex
+
     cindex.Config.set_library_file("/usr/lib/x86_64-linux-gnu/libclang-14.so.1")

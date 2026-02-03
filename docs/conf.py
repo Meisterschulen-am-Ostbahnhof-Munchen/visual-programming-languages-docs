ab5b1836f0e5f6ed48c6fb52bab96507d746f063
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2026-02-03 23:40:17 +0100

    Revert "Enable xelatex for PDF support with emojis"
    This reverts commit d2f777793b569c624ea2479a5376b8647dc57329.

--- docs/conf.py
+++ docs/conf.py
@@ -63,6 +63,3 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-# -- Options for LaTeX output ------------------------------------------------
-latex_engine = "xelatex"
